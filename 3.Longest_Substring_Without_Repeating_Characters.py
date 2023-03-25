20e16a568479f18d40c9b3de5f14935b69cf980d
hussein mansour <38099320+HusseinMansourMohd@users.noreply.github.com>
2023-03-25 08:32:13 -0500

    3.

---
+++ 3.Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,17 @@
+#https://leetcode.com/problems/longest-substring-without-repeating-characters/
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left,right = 0,0
+        seen = set()
+        maxlen = 0
+
+        while right < len(s):
+            if s[right] not in seen:
+                seen.add(s[right])
+                maxlen = max(maxlen,len(seen))
+                right +=1
+            else :
+                seen.remove(s[left])
+                left +=1
+
+        return maxlen
