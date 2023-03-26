fa4c66b0adee2a35852ec2306df52a7e33d8a59b
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-03-26 07:58:03 -0500

    finish

--- 66._Plus_One.py
+++ 66._Plus_One.py
@@ -1,13 +1,13 @@
 class Solution:
-def plusOne(self, digits: List[int]) -> List[int]:
-    n = len(digits)
-    carry = 1
-    for i in range(n-1,-1,-1):
-        digits[i] += carry
-        if digits[i] < 10:
-            carray = 0
-        else:
-            digits[i] = 0
-    if carray == 1
-        digits.insert(0,1)#0 index , 1 is what we insert
-    return digits
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+        carry = 1
+        for i in range(n-1,-1,-1):
+            digits[i] += carry
+            if digits[i] < 10:
+                carry = 0
+            else:
+                digits[i] = 0
+        if carry == 1:
+            digits.insert(0,1)#0 index , 1 is what we insert
+        return digits
