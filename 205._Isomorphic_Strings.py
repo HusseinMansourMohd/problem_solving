4c230b614475293b8482d412cd48bdda82d497a0
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-03-27 08:04:30 -0500

    second

---
+++ 205._Isomorphic_Strings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        mapping = {}
+        for i in range(len(s)):
+            if s[i] not in mapping:
+                if t[i] in mapping.values():
+                    return False
+                mapping[s[i]] = t[i]
+            else:
+                if mapping[s[i]] != t[i]:
+                    return False
+        return True
