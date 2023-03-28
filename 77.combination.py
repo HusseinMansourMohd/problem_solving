43d7dd12489ad3e36564e43ef1ea0526a6b4154d
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-03-28 13:26:13 -0500

    finish

--- 77.combination.py
+++ 77.combination.py
@@ -1,20 +1,20 @@
 Class Solution:
     def combine(self , n :int, k: int) -> List[List[int]]:
-        def backtrack(start, comb, k):
-            if k == 0:
+        def backtrack(start, comb, k): # take start , list and k number 0f premutations
+            if k == 0: # if number of premution is now zero append results
 
                 result.append(list(comb))
 
                 return
             
             for i in range(start, n+1):
-                comb.append(i)
+                comb.append(i) #append the first number 
 
-                backtrack(i+1, comb, k-1)
+                backtrack(i+1, comb, k-1)# backtrak 2 and k =1
 
                 comb.pop()
         result = []
 
         backtrack(1, [], k)
-        
+
         return result
