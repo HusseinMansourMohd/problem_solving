9c2eef4f2545c8354465fb0bef7bfbcc45484034
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-03-28 10:49:19 -0500

    edit

--- 77.combination.py
+++ 77.combination.py
@@ -2,12 +2,19 @@ Class Solution:
     def combine(self , n :int, k: int) -> List[List[int]]:
         def backtrack(start, comb, k):
             if k == 0:
+
                 result.append(list(comb))
+
                 return
+            
             for i in range(start, n+1):
                 comb.append(i)
+
                 backtrack(i+1, comb, k-1)
+
                 comb.pop()
         result = []
+
         backtrack(1, [], k)
+        
         return result
