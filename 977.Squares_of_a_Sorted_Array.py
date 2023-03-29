26a698c4dccd5f781fb39175067107c627146dd6
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-03-29 09:45:09 -0500

    number_of_steps

--- 977.Squares_of_a_Sorted_Array.py
+++ 977.Squares_of_a_Sorted_Array.py
@@ -5,3 +5,17 @@ class Solution:
         for i in range(0,len(nums)):
             new_nums.append(nums[i]*nums[i])
         return sorted(new_nums)
+    
+#second solution:
+def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        left, right = 0, n-1
+        result = [0] * n
+        for i in range(n-1, -1, -1):
+            if abs(nums[left]) > abs(nums[right]):
+                result[i] = nums[left] * nums[left]
+                left += 1
+            else:
+                result[i] = nums[right] * nums[right]
+                right -= 1
+        return result
