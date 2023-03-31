2c72d6c712b18e2283365d085804c498a5c07ad5
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-03-30 22:37:59 -0500

    finish

--- 142._Linked_List_Cycle_II.py
+++ 142._Linked_List_Cycle_II.py
@@ -6,13 +6,34 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dic = {}
-        current = head
-        count = 0
-        while current:
-            if current not in dic:
-                dic[str(current)] = count 
-                current = current.next
-            else:
-                return dic.get(str(current))
-            count += 1
+        # Check if the linked list is empty or has only one node
+        if not head or not head.next:
+            return None
+        
+        # Initialize the slow and fast pointers
+        slow = head
+        fast = head
+        
+        # Move the slow pointer one step at a time and the fast pointer two steps at a time
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+            # If the pointers meet at a node in the cycle, break out of the loop
+            if slow == fast:
+                break
+        
+        # If the fast pointer reached the end of the linked list, there is no cycle
+        if not fast or not fast.next:
+            return None
+        
+        # Reset the slow pointer to the head of the linked list
+        slow = head
+        
+        # Move both pointers one step at a time until they meet again
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+            
+        # Return the node where the cycle begins
+        return slow
