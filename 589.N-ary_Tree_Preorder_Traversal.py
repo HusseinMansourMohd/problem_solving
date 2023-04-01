9b4df8907c589442eb51f5482d1391f101052150
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-03-31 22:52:26 -0500

    1

---
+++ 589.N-ary_Tree_Preorder_Traversal.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        current = root
+        output = []
+
+        while current and current.children:
+            if current == 'null':
+                current= current.next
+            else:
+                output.append(current)
+                current = current.next
+        return output
