de73c3667b3d7c53c92afe5b6894cb05f3ee2649
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-03-31 23:21:31 -0500

    finish

--- 102.Binary_Tree_Level_Order_Traversal.py
+++ 102.Binary_Tree_Level_Order_Traversal.py
@@ -5,8 +5,21 @@ class Node:
         self.val = val
         self.children = children
 """
+
 class Solution:
     def preorder(self, root: 'Node') -> List[int]:
         if root is None:
             return []
         
+        result = []
+        stack = [root]
+        
+        while stack:
+            node = stack.pop()
+            result.append(node.val)
+            
+            # Add children from right to left so they are processed in the correct order
+            for child in reversed(node.children):
+                stack.append(child)
+        
+        return result
