148fe9ca10c5307b29bbdc46775134ae2044fcea
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-04-01 23:26:58 -0500

    finish

--- 167. Two_Sum_II-I_ Input_Array_Is_Sorted.py
+++ 167. Two_Sum_II-I_ Input_Array_Is_Sorted.py
@@ -1,13 +1,6 @@
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        
-        for first in range(len(numbers)):
-            for second in range(len(numbers) - first):
-                sum = numbers[first] + numbers[second] 
-                if sum == target:
-                    return [first+1,second+1]
-        return []
-    def two_sum_sorted(numbers, target):
+    
+    def two_sum(numbers, target):
     # Initialize two pointers: one at the beginning and one at the end of the array
     left = 0
     right = len(numbers) - 1
