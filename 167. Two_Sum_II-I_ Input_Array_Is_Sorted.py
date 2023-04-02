65d26a223410e6605b2bfdd20343e624da7b5b32
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-04-01 23:23:12 -0500

    two_pointer_solution

--- 167. Two_Sum_II-I_ Input_Array_Is_Sorted.py
+++ 167. Two_Sum_II-I_ Input_Array_Is_Sorted.py
@@ -6,4 +6,29 @@ class Solution:
                 sum = numbers[first] + numbers[second] 
                 if sum == target:
                     return [first+1,second+1]
-        return []    
+        return []
+    def two_sum_sorted(numbers, target):
+    # Initialize two pointers: one at the beginning and one at the end of the array
+    left = 0
+    right = len(numbers) - 1
+
+    # Iterate until the left pointer is less than the right pointer
+    while left < right:
+        # Calculate the sum of the elements at the pointers
+        current_sum = numbers[left] + numbers[right]
+
+        # If the current sum is equal to the target
+        if current_sum == target:
+            # Return the indices, incremented by one as the array is 1-indexed
+            return [left + 1, right + 1]
+        # If the current sum is less than the target
+        elif current_sum < target:
+            # Move the left pointer to the right
+            left += 1
+        # If the current sum is greater than the target
+        else:
+            # Move the right pointer to the left
+            right -= 1
+
+    # If no solution is found, return an empty array
+    return []    
