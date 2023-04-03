53d5475dd6fc57f9a806923a0a45bb456ca89c43
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-04-02 22:46:38 -0500

    FINISH

--- 98.Validate_Binary_Search_Tree.py
+++ 98.Validate_Binary_Search_Tree.py
@@ -11,3 +11,7 @@ class Solution:
                 return True
             if not min_val < node.val < max_val:
                 return False
+            return (validate(node.left, min_val, node.val) and
+                    validate(node.right, node.val, max_val))
+
+        return validate(root, float('-inf'), float('inf'))
