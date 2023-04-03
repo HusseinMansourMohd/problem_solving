c164d0c46379d7eb5c8b98c740f5c61577fb24bb
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-04-02 23:34:20 -0500

    finish

--- 235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
+++ 235.Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -13,7 +13,9 @@ class Solution:
 
         if p.val < root.val and q.val < root.val:
             return self.lowestCommonAncestor(root.left, p, q)
+        
         elif p.val > root.val and q.val > root.val:
             return self.lowestCommonAncestor(root.right, p, q)
+        
         else:
             return root
