dce32e18bdaedaf05fc068f020b2fc4bb5ecd1a3
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-04-03 23:22:40 -0500

    2.

--- 567.Permutation_in_String.py
+++ 567.Permutation_in_String.py
@@ -1,4 +1,10 @@
 #567. Permutation in String  #sliding window
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        from collections import Counter
+    
+        s1_len = len(s1)
+        s2_len = len(s2)
         
+        if s1_len > s2_len:
+            return False
