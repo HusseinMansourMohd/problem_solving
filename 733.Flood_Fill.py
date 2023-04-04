a94e898a38d817494e2f9238018ccd671163c5a0
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-04-03 23:44:20 -0500

    2.

--- 733.Flood_Fill.py
+++ 733.Flood_Fill.py
@@ -6,14 +6,4 @@ def floodFill(image, sr, sc, newColor):
         return image
 
     def dfs(row, col):
-        if row < 0 or row >= rows or col < 0 or col >= cols or image[row][col] != start_color:
-            return
-
-        image[row][col] = newColor
-        dfs(row - 1, col)
-        dfs(row + 1, col)
-        dfs(row, col - 1)
-        dfs(row, col + 1)
-
-    dfs(sr, sc)
-    return image
+        
