d04a0eda35d5975a625f84dc3fcd2da01c1083bb
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-04-04 23:34:53 -0500

    finish

--- 200.Number_of_Islands.py
+++ 200.Number_of_Islands.py
@@ -33,5 +33,35 @@
 # 1 <= m, n <= 300
 # grid[i][j] is '0' or '1'.
 
+# 1/Initialize a variable count to store the number of islands.
+# 2/Loop through each cell in the grid:
+# a. If the current cell is '1', increment the count variable and perform DFS to mark all connected land cells as '0' (visited).
+# b. Continue to the next cell.
+# After traversing the entire grid, return the count variable as the number of islands.
+
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        row = len(grid)
+        col = len(grid[0])
+        
+
+        def dfs(grid, row, col):
+            if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] == '0':
+                return
+            grid[row][col] = '0'  #mark cell as visited
+
+            #preform DFS on adjacent cells
+            dfs(grid, row - 1 , col)
+            dfs(grid, row + 1 , col)
+            dfs(grid, row , col - 1)
+            dfs(grid, row , col + 1)
+            
+        count = 0
+        for i in range(rows):
+            for j in range(col):
+                if grid[i][j] == '1':
+                    count += 1
+                    dfs(gird,i,j)
+        return count
+
+        
