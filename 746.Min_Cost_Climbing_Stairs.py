36dbde684a35a39692d1fea3c1e0a865e87f3d98
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-04-05 23:45:16 -0500

    1,

--- 746.Min_Cost_Climbing_Stairs.py
+++ 746.Min_Cost_Climbing_Stairs.py
@@ -1,3 +1,8 @@
 class Solution:
-    def minCostClimbingStairs(self, cost:List[int]) -> int:
-        
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n+1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
