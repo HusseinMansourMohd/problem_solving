fdb4613b37bd5b91375fd8b090a6f15c8f826335
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-04-06 23:49:32 -0500

    3.

--- 62.Unique_Paths.py
+++ 62.Unique_Paths.py
@@ -2,3 +2,5 @@ class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         # Create a 2D DP array with dimensions m x n, initialized to 1
         dp = [[1] * n for _ in range(m)]
+        # Iterate through the grid starting from the second row and second column
+        for i in range(1, m):
