fd806883a1ae558ef4acccb36c0263ea3d5d0338
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-04-06 23:50:41 -0500

    4.

--- 62.Unique_Paths.py
+++ 62.Unique_Paths.py
@@ -4,3 +4,6 @@ class Solution:
         dp = [[1] * n for _ in range(m)]
         # Iterate through the grid starting from the second row and second column
         for i in range(1, m):
+            for j in range(1, n):
+            # The number of unique paths to cell (i, j) is the sum of unique paths to cells (i - 1, j) and (i, j - 1)
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
