988fb50fa3d2e0ac105e8a411683ec50f8ea8161
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-04-13 23:53:34 -0500

    finish

--- 278.First_Bad_Version.py
+++ 278.First_Bad_Version.py
@@ -2,6 +2,7 @@ class Solution:
     def firstBadVersion(self, n: int) -> int:
         left = 1
         right = n
+        #####
         while (left < right) :
             mid = left + (right - left) // 2
             if (isBadVersion(mid)):
