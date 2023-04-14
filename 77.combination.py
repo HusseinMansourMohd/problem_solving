30040926a4ab96ad8ce545477df1e1e7a7724259
HusseinMansourMohd <husseinmcwii13@gmail.com>
2023-04-13 23:52:40 -0500

    finidh

--- 77.combination.py
+++ 77.combination.py
@@ -2,7 +2,7 @@ Class Solution:
     def combine(self , n :int, k: int) -> List[List[int]]:
         def backtrack(start, comb, k): # take start , list and k number 0f premutations
             if k == 0: # if number of premution is now zero append results
-
+                #####
                 result.append(list(comb))
 
                 return
